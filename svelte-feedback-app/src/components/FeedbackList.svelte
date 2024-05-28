900b271d1d5806efa7b4194863788250a3ea487a
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-05-28 17:10:15 +0800

    it is finished

---
+++ svelte-feedback-app/src/components/FeedbackList.svelte
@@ -0,0 +1,24 @@
+<script>
+    // import { onMount, onDestroy } from 'svelte'
+    import { FeedbackStore } from '../stores'
+    import { fade, scale } from 'svelte/transition'
+    import FeedbackItem from './FeedbackItem.svelte'
+    
+    // let feedback = []
+
+
+    // const unsubscribed = FeedbackStore.subscribe((data) => feedback = data)
+
+    // onMount(() => {
+    //     console.log('mounted')
+    // })
+    // onDestroy(() => {
+    //    unsubscribed()
+    // })
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+<div in:scale out:fade="{{ duration: 500 }}">
+<FeedbackItem item={fb} />
+</div>
+{/each}
